74ebbb20c3c347064f56108cd1d37475dadcb931
craftzcode <tomymaritano@gmail.com>
2022-06-27 12:57:16 +0200

    moving js files

--- js/arrays_criptos.js
+++ js/arrays_criptos.js
@@ -1,40 +1,44 @@
-// const cryptocurrencie = [
-// {
-//     name: 'Bitcoin',
-//     id: 1,
-//     value: 19250,
-// },
-// {
-//     name: 'Etherum',
-//     id: 2,
-//     value: 1400,
-// },
-// {
-//     name: 'XPR',
-//     id: 3,
-//     value: 0.3,
-// },
-// {
-//     name: 'Link',
-//     id: 4,
-//     value: 6.8,
-// },
-// {
-//     name: 'LTC',
-//     id: 5,
-//     value: 52,
-// }
-// ];
+//Array
+const cryptocurrencie = [
+  {
+    name: "Bitcoin",
+    id: 1,
+    value: 19250,
+  },
+  {
+    name: "Etherum",
+    id: 2,
+    value: 1400,
+  },
+  {
+    name: "XPR",
+    id: 3,
+    value: 0.3,
+  },
+  {
+    name: "ADA",
+    id: 4,
+    value: 6.8,
+  },
+  {
+    name: "LiteCoin",
+    id: 5,
+    value: 52,
+  },
+];
 
+//Constructor
 
+class Cryptos {
+  constructor(name, id, value) {
+    this.name = name;
+    this.id = id;
+    this.value = value;
+  }
+}
 
+cryptocurrencie.push(new Cryptos("EOS", 3, 30));
+cryptocurrencie.push(new Cryptos("XPR", 4, 25));
 
-// class Cryptos {
-//     constructor (name , id, value) {
-//         this.name = name;
-//         this.id = id;
-//         this.value = value;
-//     }
-// }
-
-// cryptocurrencie.push (new Cryptos('EOS', 3, 30));
+// console.log(cryptocurrencie);
+// console.log(cryptocurrencie.length);
