5b279c3f5c57784d12b5e703b3570e10bc7462b0
craftzcode <tomymaritano@gmail.com>
2022-06-30 11:27:59 +0200

    updates

--- js/functions.js
+++ js/functions.js
@@ -13,13 +13,14 @@ const inputNumber = (e) => {
 
 //IVA
 const iva = (x) => x * 0.21;
-const taxes = (a, b, c) => {  
-  return ((a - b) / c ); 
+const taxes = (a, b, c) => {
+  return (a - b) / c;
 };
 
 //Bank transfers
-const sepa = x => { x-- };
+const sepa = (x) => {
+  x--;
+};
 const visaFee = (x) => {
   x * 0.18;
 };
-
--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ window.onload = () => {
   form.onsubmit = (e) => {
     e.preventDefault();
 
+    //Getting investment ammount
     const charginAmmount = () => {
       const ammount = document.getElementById("ammount");
       const valueAmmount = ammount.value;
@@ -11,19 +12,22 @@ window.onload = () => {
       //   "Estas invirtiendo un total de " + " " + valueAmmount + " " + "USD",
       // );
 
+      //Getting crypto category
       const cryptoCategory = () => {
         const selectCrypto = document.getElementById("selectedCrypto");
         const selectedCrypto =
           selectCrypto.options[selectCrypto.selectedIndex].value;
         // console.log(selectedCrypto);
 
-        //ENGINE
+        //Engine function and parseFloat function with toFixed() to get 3 decimals;
         const engineFunction = () => {
           for (const cryptos of cryptoConcat) {
             if (cryptos.name === selectedCrypto && cryptos.id > 0) {
-              return taxes(valueAmmount, iva(valueAmmount), cryptos.value);
-            }
-          }
+              return parseFloat(
+                taxes(valueAmmount, iva(valueAmmount), cryptos.value),
+              ).toFixed(3)
+            };
+          };
         };
         const buttonFunction = () => {
           const buyButton = document
@@ -37,13 +41,14 @@ window.onload = () => {
 
           const transactionFunction = (value) => {
             let add = document.createElement("div");
+            add.classList.add("container");
             add.textContent = value;
             return add;
           };
 
           transactionHistory.appendChild(
-            transactionFunction(`${engineFunction()}, ${selectCrypto.value}
-            `),
+            transactionFunction(`Total ${selectCrypto.value} = ${engineFunction()}
+            `)
           );
         };
 
