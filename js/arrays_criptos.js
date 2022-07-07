f553c913f79219384e2316c4cd78f1a32428ac7e
craftzcode <tomymaritano@gmail.com>
2022-07-07 23:01:18 +0200

    upd

--- js/arrays_criptos.js
+++ js/arrays_criptos.js
@@ -21,7 +21,7 @@ const cryptocurrencie = [
     abbr: 'XPR',
     id: 3,
     value: 0.3,
-    mkt: 35.0,
+    mkt: 351220,
     type: 'ETF',
   },
   {
@@ -29,7 +29,7 @@ const cryptocurrencie = [
     abbr: 'ADA',
     id: 4,
     value: 6.8,
-    mkt: 35.0,
+    mkt: 33220,
     type: 'POS',
   },
   {
@@ -37,7 +37,7 @@ const cryptocurrencie = [
     abbr: 'LTC',
     id: 5,
     value: 52,
-    mkt: 35.0,
+    mkt: 33220,
     type: 'POS',
   },
 ];
@@ -47,7 +47,6 @@ const cryptocurrencieClass = [];
 // console.log(cryptocurrencieClass)
 
 //Constructor increasing id automatically
-
 class Cryptos {
   static count = 5;
   constructor(name, abbr, value, mkt, type) {
@@ -60,78 +59,86 @@ class Cryptos {
   }
 }
 
-//Va
-
-
 //NFT
-cryptocurrencieClass.push(new Cryptos("My neighbor Alice", "ALICE", 2.48, 74.88, 'NFT'));
-cryptocurrencieClass.push(new Cryptos("ApeCoin", "APE", 5.13, 1.537, 'NFT'));
-cryptocurrencieClass.push(new Cryptos("Axie Infinity", "AXS", 15.92, 1.261, 'NFT'));
-cryptocurrencieClass.push(new Cryptos("Bakery Token", "BAKE", 0.300, 58.07, 'NFT'));
-cryptocurrencieClass.push(new Cryptos("BinaryX", "BNX", 118.20, 239.10, 'NFT'));
-cryptocurrencieClass.push(new Cryptos("Chromia", "CRX", 0.190, 108.34, 'NFT'));
-cryptocurrencieClass.push(new Cryptos("SuperRare", "RARE",0.24 , 25.13, 'NFT'));
-cryptocurrencieClass.push(new Cryptos("Green Token Metaverse", "GTM", 0.90, 589.15, 'NFT'));
-cryptocurrencieClass.push(new Cryptos("HighStreet", "HIGH", 1.45, 17.83, 'NFT'));
-cryptocurrencieClass.push(new Cryptos("Immutable X", "IMX",  1.01, 237.63, 'NFT'));
-cryptocurrencieClass.push(new Cryptos("Decentraland", "MANA", 0.93, 1.726, 'NFT'));
-cryptocurrencieClass.push(new Cryptos("OriginToken", "OGN", 0.26, 106.70, 'NFT'));
-cryptocurrencieClass.push(new Cryptos("The SandBox", "SAND", 0.45, 53.43, 'NFT'));
-cryptocurrencieClass.push(new Cryptos("Dego Finance", "1.62", 1.62, 13.94, 'NFT'));
-cryptocurrencieClass.push(new Cryptos("Enjin Coin", "ENJ", 0.56, 502.32, 'NFT'));
-cryptocurrencieClass.push(new Cryptos("Ethernity Chain", "ERN", 1.59, 24.60, 'NFT'));
-
+cryptocurrencieClass.push(new Cryptos("My neighbor Alice", "ALICE", 2.48, 7488000, 'NFT'));
+cryptocurrencieClass.push(new Cryptos("ApeCoin", "APE", 5.13, 15370000, 'NFT'));
+cryptocurrencieClass.push(new Cryptos("Axie Infinity", "AXS", 15.92, 1261100, 'NFT'));
+cryptocurrencieClass.push(new Cryptos("Bakery Token", "BAKE", 0.300, 5807300, 'NFT'));
+cryptocurrencieClass.push(new Cryptos("BinaryX", "BNX", 118.20, 23910100, 'NFT'));
+cryptocurrencieClass.push(new Cryptos("Chromia", "CRX", 0.190, 1083200, 'NFT'));
+cryptocurrencieClass.push(new Cryptos("SuperRare", "RARE",0.24 , 2513300, 'NFT'));
+cryptocurrencieClass.push(new Cryptos("Green Token Metaverse", "GTM", 0.90, 5891500, 'NFT'));
+cryptocurrencieClass.push(new Cryptos("HighStreet", "HIGH", 1.45, 1783232, 'NFT'));
+cryptocurrencieClass.push(new Cryptos("Immutable X", "IMX",  1.01, 2376323, 'NFT'));
+cryptocurrencieClass.push(new Cryptos("Decentraland", "MANA", 0.93, 1726023, 'NFT'));
+cryptocurrencieClass.push(new Cryptos("OriginToken", "OGN", 0.26, 10670323, 'NFT'));
+cryptocurrencieClass.push(new Cryptos("The SandBox", "SAND", 0.45, 532392, 'NFT'));
+cryptocurrencieClass.push(new Cryptos("Dego Finance", "1.62", 1.62, 139499, 'NFT'));
+cryptocurrencieClass.push(new Cryptos("Enjin Coin", "ENJ", 0.56, 50232932, 'NFT'));
+cryptocurrencieClass.push(new Cryptos("Ethernity Chain", "ERN", 1.59, 2460932, 'NFT'));
 
 //POS
-cryptocurrencieClass.push(new Cryptos("AERGO", "Aergo", 0.45, 53.43, 'POS'));
-cryptocurrencieClass.push(new Cryptos("AION", "AION", 0.13, 53.47, 'POS'));
-cryptocurrencieClass.push(new Cryptos("Algorand", "ALGO", 0.31, 2.210, 'POS'));
-cryptocurrencieClass.push(new Cryptos("Ambrous", "AMBO", 0.0005, 5.17, 'POS'));
-cryptocurrencieClass.push(new Cryptos("Ankr", "ANKR", 0.0029, 238.82, 'POS'));
-cryptocurrencieClass.push(new Cryptos("Aragon", "ANT", 1.73, 68.52, 'POS'));
-cryptocurrencieClass.push(new Cryptos("Ardor", "ARDR",  0.10, 104.98, 'POS'));
-cryptocurrencieClass.push(new Cryptos("Ark", "ARK", 0.42, 59.09, 'POS'));
-cryptocurrencieClass.push(new Cryptos("Cosmos", "ATOS", 9.09, 2.603, 'POS'));
-cryptocurrencieClass.push(new Cryptos("Avalanche", "AVAX", 19.84, 5.622, 'POS'));
-cryptocurrencieClass.push(new Cryptos("BAND", "BAND", 1.4, 49.25, 'POS'));
-cryptocurrencieClass.push(new Cryptos("BNB", "BNB", 241.1, 39.394, 'POS'));
-cryptocurrencieClass.push(new Cryptos("BitShares", "BTS", 0.01, 33.11, 'POS'));
-cryptocurrencieClass.push(new Cryptos("BitTorrent", "BTTC", 0.0, 934.943, 'POS'));
-cryptocurrencieClass.push(new Cryptos("Clover Finance", "CLV", 0.09, 30.66, 'POS'));
-cryptocurrencieClass.push(new Cryptos("Cocos-BCX", "COCOS", 0.74, 49.93, 'POS'));
-cryptocurrencieClass.push(new Cryptos("Shentu", "CTK", 0.84, 89.98, 'POS'));
-cryptocurrencieClass.push(new Cryptos("Cartesei", "CTSI", 0.153, 89.09, 'POS'));
-cryptocurrencieClass.push(new Cryptos("Dash", "DASH", 45.20, 488.53, 'POS'));
-cryptocurrencieClass.push(new Cryptos("Decred", "DCR", 22.40, 317.22, 'POS'));
-cryptocurrencieClass.push(new Cryptos("DIA", "DIA", 0.41, 33.28, 'POS'));
-cryptocurrencieClass.push(new Cryptos("DOT", "Polkadot", 7.15, 7.061, 'POS'));
+cryptocurrencieClass.push(new Cryptos("AERGO", "Aergo", 0.45, 53343, 'POS'));
+cryptocurrencieClass.push(new Cryptos("AION", "AION", 0.13, 53247, 'POS'));
+cryptocurrencieClass.push(new Cryptos("Algorand", "ALGO", 0.31, 21210, 'POS'));
+cryptocurrencieClass.push(new Cryptos("Ambrous", "AMBO", 0.0005, 2317, 'POS'));
+cryptocurrencieClass.push(new Cryptos("Ankr", "ANKR", 0.0029, 2383282, 'POS'));
+cryptocurrencieClass.push(new Cryptos("Aragon", "ANT", 1.73, 6823252, 'POS'));
+cryptocurrencieClass.push(new Cryptos("Ardor", "ARDR",  0.10, 1042398, 'POS'));
+cryptocurrencieClass.push(new Cryptos("Ark", "ARK", 0.42, 592309, 'POS'));
+cryptocurrencieClass.push(new Cryptos("Cosmos", "ATOS", 9.09, 2603, 'POS'));
+cryptocurrencieClass.push(new Cryptos("Avalanche", "AVAX", 19.84, 5622, 'POS'));
+cryptocurrencieClass.push(new Cryptos("BAND", "BAND", 1.4, 4925, 'POS'));
+cryptocurrencieClass.push(new Cryptos("BNB", "BNB", 241.1, 39394, 'POS'));
+cryptocurrencieClass.push(new Cryptos("BitShares", "BTS", 0.01, 3311, 'POS'));
+cryptocurrencieClass.push(new Cryptos("BitTorrent", "BTTC", 0.0, 934943, 'POS'));
+cryptocurrencieClass.push(new Cryptos("Clover Finance", "CLV", 0.09, 3066, 'POS'));
+cryptocurrencieClass.push(new Cryptos("Cocos-BCX", "COCOS", 0.74, 4993, 'POS'));
+cryptocurrencieClass.push(new Cryptos("Shentu", "CTK", 0.84, 8998, 'POS'));
+cryptocurrencieClass.push(new Cryptos("Cartesei", "CTSI", 0.153, 8909, 'POS'));
+cryptocurrencieClass.push(new Cryptos("Dash", "DASH", 45.20, 48853, 'POS'));
+cryptocurrencieClass.push(new Cryptos("Decred", "DCR", 22.40, 31722, 'POS'));
+cryptocurrencieClass.push(new Cryptos("DIA", "DIA", 0.41, 3328, 'POS'));
+cryptocurrencieClass.push(new Cryptos("DOT", "Polkadot", 7.15, 7061, 'POS'));
 
 //FANTOKEN
-cryptocurrencieClass.push(new Cryptos("AC Milan", "ACM", 2.84000, 8.94, 'Fan Token'));
-cryptocurrencieClass.push(new Cryptos("Alpine", "F1 ALPINE", 2.45, 27.32, 'Fan Token'));
-cryptocurrencieClass.push(new Cryptos("AS ROMA", "ASR", 2.95, 6.38, 'Fan Token'));
-cryptocurrencieClass.push(new Cryptos("Atletico Madrid", "ATM", 3.19, 6.65, 'Fan Token'));
-cryptocurrencieClass.push(new Cryptos("FC Barcelona", "BAR", 4.07, 16.00, 'Fan Token'));
-cryptocurrencieClass.push(new Cryptos("Chiliz", "CHZ", 0.10, 608.43, 'Fan Token'));
-cryptocurrencieClass.push(new Cryptos("Manchester City", "CITY", 4.52, 15.86, 'Fan Token'));
-cryptocurrencieClass.push(new Cryptos("Juventus", "JUV", 3.84, 5.02, 'Fan Token'));
-cryptocurrencieClass.push(new Cryptos("LAZIO", "LAZIO", 2.14, 18.40, 'Fan Token'));
-cryptocurrencieClass.push(new Cryptos("OG", "OG", 3.48, 4.43, 'Fan Token'));
-cryptocurrencieClass.push(new Cryptos("FC PORTO", "PORTO", 1.81, 14.11, 'Fan Token'));
-cryptocurrencieClass.push(new Cryptos("Paris Saint Germain", "PSG", 6.28, 19.53, 'Fan Token'));
-cryptocurrencieClass.push(new Cryptos("Santo FC", "SANTOS", 3.73, 16.97, 'Fan Token'));
+cryptocurrencieClass.push(new Cryptos("AC Milan", "ACM", 2.84000, 894, 'Fan Token'));
+cryptocurrencieClass.push(new Cryptos("Alpine", "F1 ALPINE", 2.45, 2732, 'Fan Token'));
+cryptocurrencieClass.push(new Cryptos("AS ROMA", "ASR", 2.95, 638, 'Fan Token'));
+cryptocurrencieClass.push(new Cryptos("Atletico Madrid", "ATM", 3.19, 665, 'Fan Token'));
+cryptocurrencieClass.push(new Cryptos("FC Barcelona", "BAR", 4.07, 1600, 'Fan Token'));
+cryptocurrencieClass.push(new Cryptos("Chiliz", "CHZ", 0.10, 60843, 'Fan Token'));
+cryptocurrencieClass.push(new Cryptos("Manchester City", "CITY", 4.52, 1586, 'Fan Token'));
+cryptocurrencieClass.push(new Cryptos("Juventus", "JUV", 3.84, 502, 'Fan Token'));
+cryptocurrencieClass.push(new Cryptos("LAZIO", "LAZIO", 2.14, 1840, 'Fan Token'));
+cryptocurrencieClass.push(new Cryptos("OG", "OG", 3.48, 443, 'Fan Token'));
+cryptocurrencieClass.push(new Cryptos("FC PORTO", "PORTO", 1.81, 1411, 'Fan Token'));
+cryptocurrencieClass.push(new Cryptos("Paris Saint Germain", "PSG", 6.28, 1953, 'Fan Token'));
+cryptocurrencieClass.push(new Cryptos("Santo FC", "SANTOS", 3.73, 1697, 'Fan Token'));
+
+//STORAGE
+cryptocurrencieClass.push(new Cryptos("SONM", "SNM", 0.05, 2500000, 'Storage'));
+cryptocurrencieClass.push(new Cryptos("Holo", "HOT", 0.002, 371000000, 'Storage'));
+cryptocurrencieClass.push(new Cryptos("SiaCoin", "SC", 3.73, 16.97, 'Storage'));
+cryptocurrencieClass.push(new Cryptos("RSK Infraestructure Network", "RIF", 0.05, 203000000, 'Storage'));
+cryptocurrencieClass.push(new Cryptos("Bluzelle", "BLZ", 0.091, 30000000, 'Storage'));
+cryptocurrencieClass.push(new Cryptos("Stacks", "STX", 0.040, 53667000, 'Storage'));
+cryptocurrencieClass.push(new Cryptos("Storj", "STORJ", 0.76, 30410000, 'Storage'));
+cryptocurrencieClass.push(new Cryptos("Prometeus", "PROM", 5.33, 87670000, 'Storage'));
+cryptocurrencieClass.push(new Cryptos("FileCoin", "FILE", 1324080000, 16.97, 'Storage'));
 
 
 
 //Money Format
-function moneyFormat(price, sign = '$') {
+const moneyFormat = (price, sign = '$') => {
   const pieces = parseFloat(price).toFixed(2).split('')
   let ii = pieces.length - 3
   while ((ii-=3) > 0) {
     pieces.splice(ii, 0, ',')
   }
   return sign + pieces.join('')
-}
+};
+
 
 // console.log(cryptocurrencie);
 // console.log(cryptocurrencie.length);
@@ -170,7 +177,7 @@ const cryptocurrencieCopyMap = cryptocurrencieCopy.map((x) => {
 
 //Concat array class and main array
 const cryptoConcat = cryptocurrencieMap.concat(cryptocurrencieCopyMap);
-console.log(cryptoConcat);
+// console.log(cryptoConcat);
 
 const cryptoConcatCopy = [...cryptoConcat]
 // console.log(cryptoConcatCopy)
@@ -188,4 +195,60 @@ const cryptoConcatCopy = [...cryptoConcat]
 const totalCryptoValue = cryptoConcat.reduce((a, b) => a + b.value, 0);
 // console.log(totalCryptoValue);
 
+// //MAGIC
+// let imgString = [];
+// console.log(imgString);
+// let iterator = imgString.values();
+// console.log(imgString);
 
+// const magic = () => {
+//   for (let i = 0; i < cryptoConcat.length; i++) {
+//     let dir = '../img/icons/'
+//     const element = cryptoConcat[i].id;
+//     imgString.push(dir+element+'.svg')
+//   }
+// }
+// magic();
+
+
+//   const imageStringX = () => {
+//     for (const item of cryptoConcat) {
+//       // console.log(item.id)
+//       let comp = item.id;
+//       return comp
+//     }
+//   }
+//   imageStringX();
+
+//   const imageStringZ = () => {
+//     for (let i = 1; i < imgString.length; i++) {
+//       const element = [i++];
+//       return console.log(element)
+//     } 
+//   }
+//   imageStringZ();
+
+//   const r = () => {
+//     if (imageStringX() == imageStringZ()){ 
+//       return imgString[true] 
+//     }
+//   }
+// r();
+
+
+// const prueba = () => {
+//   for (const i of iterator) {
+//     while(true){
+//       if(imageStringX() === imageStringZ()) {
+//         return console.log()
+//       }
+//     }
+//     };
+//     }
+
+
+prueba();
+// if(imageStringX() != imageStringZ()) {
+//   while(true) {
+//     return console.log(i)
+//   }
--- js/market.js
+++ js/market.js
@@ -1,5 +1,5 @@
 let market = document.getElementById('market')
-console.log(market)
+// console.log(market)
 
 const marketList = () => {
     for (const item of cryptoConcat) {
@@ -7,6 +7,19 @@ const marketList = () => {
             const columns = document.createElement('div');
                 columns.classList.add('columns', 'border-bottom', 'is-mobile', 'is-size-7')
                 market.appendChild(columns);
+
+            let spanImg = document.createElement('span');
+                spanImg.classList.add('pl-3', 'is-size-7', 'has-text-dark', 'has-text-weight-light', 'addingImg', 'is-flex')
+            // let imageToSpan = document.createElement('img')
+            //     imageToSpan.src = prueba();
+            //     console.log(imageToSpan)
+                columns.appendChild(spanImg);
+                // spanImg.appendChild(imageToSpan);
+
+                
+            
+            let addingImg = document.getElementsByClassName('addingImg');
+                addingImg.src
     
             let columnName = document.createElement('div')
                 columnName.classList.add('column', 'is-size-6', 'has-text-weight-bold' );
@@ -29,17 +42,17 @@ const marketList = () => {
                 columns.appendChild(columnId);
     
             let columnMkt = document.createElement('div')
-                columnMkt.classList.add('column');
+                columnMkt.classList.add('column', 'has-text-weight-bold');
                 columnMkt.textContent = moneyFormat(item.mkt);
                 columns.appendChild(columnMkt);
         };
-        columnsMarket() 
+        columnsMarket();
+
+
     }
     //     document.getElementById('clickname').onchange = () => {
     //     return console.log('Hola')
     // }; 
-
-
 }
 marketList();
 
