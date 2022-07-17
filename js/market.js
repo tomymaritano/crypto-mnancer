2c6e7be30fb5c7393e773be8c1d1948e4f32a072
craftzcode <tomymaritano@gmail.com>
2022-07-17 17:57:29 +0200

    msj

--- js/market.js
+++ js/market.js
@@ -57,19 +57,19 @@ const highlight = () => {
     document.getElementById('btc-img').src = cryptoJson[0].img
     document.getElementById('span-bitcoin').textContent = data.data[0].name;
     document.querySelector('.btc-highlight-value').textContent = moneyFormat(data.data[0].priceUsd);
-    document.querySelector('.btc-highlight-mkt').textContent = moneyFormat(data.data[0].supply);
+    document.querySelector('.btc-highlight-mkt').textContent = data.data[0].supply;
 
     //ETC
     document.getElementById('etc-img').src = cryptoJson[1].img
     document.getElementById('span-ether').textContent = data.data[1].name;
     document.querySelector('.etc-highlight-value').textContent = moneyFormat(data.data[1].priceUsd);
-    document.querySelector('.etc-highlight-mkt').textContent = moneyFormat(data.data[1].supply)
+    document.querySelector('.etc-highlight-mkt').textContent = data.data[1].supply;
 
     //XPRR
     document.getElementById('xpr-img').src = cryptoJson[2].img;
     document.getElementById('span-xpr').textContent = data.data[2].name
     document.querySelector('.xpr-highlight-value').textContent = moneyFormat(data.data[2].priceUsd)
-    document.querySelector('.xpr-highlight-mkt').textContent = moneyFormat(data.data[2].supply)
+    document.querySelector('.xpr-highlight-mkt').textContent = data.data[2].supply
 };
 highlight();
 
@@ -78,66 +78,65 @@ const newListing = () => {
     document.getElementById('ltc-img').src = cryptoJson[5].img;
     document.getElementById('span-ltc').textContent = data.data[3].symbol
     document.querySelector('.ltc-highlight-value').textContent = moneyFormat(data.data[3].priceUsd)
-    document.querySelector('.ltc-highlight-mkt').textContent = moneyFormat(data.data[3].supply)
+    document.querySelector('.ltc-highlight-mkt').textContent = data.data[3].supply
 
     //kETC
     document.getElementById('ada-img').src = cryptoJson[3].img;
     document.getElementById('span-ada').textContent = data.data[4].symbol
     document.querySelector('.ada-highlight-value').textContent = moneyFormat(data.data[4].priceUsd)
-    document.querySelector('.ada-highlight-mkt').textContent = moneyFormat(data.data[4].priceUsd)
+    document.querySelector('.ada-highlight-mkt').textContent = data.data[4].supply
 
     //XPRR
     document.getElementById('ape-img').src = cryptoJson[6].img;
     document.getElementById('span-ape').textContent = data.data[5].symbol
     document.querySelector('.ape-highlight-value').textContent = moneyFormat(data.data[5].priceUsd)
-    document.querySelector('.ape-highlight-mkt').textContent = moneyFormat(data.data[5].supply)
+    document.querySelector('.ape-highlight-mkt').textContent = data.data[5].supply
 };
 newListing();
 
 const topGainerCoin = () => {
     //BTC
     document.getElementById('axie-img').src = cryptoJson[7].img;
-    document.getElementById('span-axie').textContent = cryptoJson[7].abbr;
-    document.querySelector('.axie-highlight-value').textContent = cryptoJson[7].value;
-    document.querySelector('.axie-highlight-mkt').textContent = moneyFormat(cryptoJson[7].mkt);
+    document.getElementById('span-axie').textContent = data.data[7].symbol;
+    document.querySelector('.axie-highlight-value').textContent = moneyFormat(data.data[7].priceUsd);
+    document.querySelector('.axie-highlight-mkt').textContent = data.data[7].supply;
 
     //ETC
     document.getElementById('bake-img').src = cryptoJson[8].img;
-    document.getElementById('span-bake').textContent = cryptoJson[8].abbr;
-    document.querySelector('.bake-highlight-value').textContent = cryptoJson[8].value;
-    document.querySelector('.bake-highlight-mkt').textContent = moneyFormat(cryptoJson[8].mkt);
+    document.getElementById('span-bake').textContent = data.data[8].symbol;
+    document.querySelector('.bake-highlight-value').textContent = moneyFormat(data.data[8].priceUsd);
+    document.querySelector('.bake-highlight-mkt').textContent = data.data[8].supply
 
     //XPRR
     document.getElementById('rare-img').src = cryptoJson[9].img;
-    document.getElementById('span-rare').textContent = cryptoJson[9].abbr;
-    document.querySelector('.rare-highlight-value').textContent = cryptoJson[9].value;
-    document.querySelector('.rare-highlight-mkt').textContent = moneyFormat(cryptoJson[9].mkt);
+    document.getElementById('span-rare').textContent = data.data[9].symbol;
+    document.querySelector('.rare-highlight-value').textContent = moneyFormat(data.data[9].priceUsd);
+    document.querySelector('.rare-highlight-mkt').textContent = data.data[9].supply
 };
 topGainerCoin();
 
 const topVolumeCoin = () => {
     //BTC
     document.getElementById('ankr-img').src = cryptoJson[25].img;
-    document.getElementById('span-ankr').textContent = cryptoJson[25].abbr;
-    document.querySelector('.ankr-highlight-value').textContent = cryptoJson[0].value;
-    document.querySelector('.ankr-highlight-mkt').textContent = moneyFormat(cryptoJson[0].mkt);
+    document.getElementById('span-ankr').textContent = data.data[25].symbol;
+    document.querySelector('.ankr-highlight-value').textContent = moneyFormat(data.data[0].priceUsd);
+    document.querySelector('.ankr-highlight-mkt').textContent = data.data[0].supply;
 
     //ETC
     document.getElementById('ant-img').src = cryptoJson[26].img;
-    document.getElementById('span-ant').textContent = cryptoJson[26].abbr;
-    document.querySelector('.ant-highlight-value').textContent = cryptoJson[26].value;
-    document.querySelector('.ant-highlight-mkt').textContent = moneyFormat(cryptoJson[26].mkt);
+    document.getElementById('span-ant').textContent = data.data[26].symbol;
+    document.querySelector('.ant-highlight-value').textContent = moneyFormat(data.data[26].priceUsd);
+    document.querySelector('.ant-highlight-mkt').textContent = data.data[26].supply;
 
     //XPRR
     document.getElementById('ardr-img').src = cryptoJson[27].img;
-    document.getElementById('span-ardr').textContent = cryptoJson[27].abbr;
-    document.querySelector('.ardr-highlight-value').textContent = cryptoJson[27].value;
-    document.querySelector('.ardr-highlight-mkt').textContent = moneyFormat(cryptoJson[27].mkt);
+    document.getElementById('span-ardr').textContent = data.data[27].symbol;
+    document.querySelector('.ardr-highlight-value').textContent = moneyFormat(data.data[27].priceUsd);
+    document.querySelector('.ardr-highlight-mkt').textContent = data.data[27].supply;
 };
 topVolumeCoin();
 
                 }
-
             }
             marketTopNavbar();
 
